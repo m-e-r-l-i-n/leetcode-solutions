86ca534b6da79cd114a881832c0c048abb5b9ac5
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-06-15 16:10:44 +0530

    Create 700.Search in a Binary Search Tree.java

---
+++ algorithms/700.Search in a Binary Search Tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null) return null;
+        if(root.val==val) return root;
+        TreeNode n1=searchBST(root.left,val),n2=searchBST(root.right,val);
+        if(n1!=null) return n1;
+        else return n2;
+    }
+}
