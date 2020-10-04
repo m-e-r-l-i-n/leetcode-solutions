b6293f714ae4d8b8bd54d84216d5c9c817ceaa29
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-10-05 03:01:09 +0530

    Update 1288.Remove Covered Intervals.java

--- algorithms/1288.Remove Covered Intervals.java
+++ algorithms/1288.Remove Covered Intervals.java
@@ -2,12 +2,12 @@ class Solution {
     public int removeCoveredIntervals(int[][] inter) {
         int n=inter.length;
         if(n<=1) return n;
-		Arrays.sort(inter, (a,b)->a[0]==b[0]?b[1]-a[1]:a[0]-b[0]);
-		int c=1,i,prev[]=inter[0];
-        for(i=1;i<n;i++) 
+        Arrays.sort(inter, (a,b)->a[0]==b[0]?b[1]-a[1]:a[0]-b[0]);
+        int c=1,i,prev[]=inter[0];
+        for(i=1;i<n;i++)
         {
             int[] next=inter[i];
-			if(prev[0]<=next[0] && prev[1]>=next[1]) continue;
+            if(prev[0]<=next[0] && prev[1]>=next[1]) continue;
             prev=next;
             c++;
         }
