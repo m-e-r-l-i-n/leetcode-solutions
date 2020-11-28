7f936fcda5a9d923da7dfe4fcd28f05a0ca427bb
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-11-28 17:14:18 +0530

    Create 45.Jump Game II.java

---
+++ algorithms/45.Jump Game II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] a) 
+    {
+        int i,ans=0,cur=0,max=0;
+	    for(i=0;i<a.length-1; i++) 
+        {
+		    max=Math.max(max,i+a[i]);
+		    if(i==cur) 
+            {
+			    ans++;
+			    cur=max;
+		    }
+	    }
+	    return ans;        
+    }
+}
