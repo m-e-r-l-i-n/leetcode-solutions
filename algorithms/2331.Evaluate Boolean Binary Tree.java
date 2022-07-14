600437ed5d6883d689ba2eed92d00325431090d3
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2022-07-14 12:46:20 +0530

    Create 2331.Evaluate Boolean Binary Tree.java

---
+++ algorithms/2331.Evaluate Boolean Binary Tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.left==null && root.right==null) return root.val==1;
+        
+        boolean lv=evaluateTree(root.left),rv=evaluateTree(root.right);
+        if(root.val==2) return lv|rv;
+        return lv&rv;
+    }
+}
