61529d5583e8fa4bb3de77dc7324928e5426fa1a
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2022-08-05 14:43:35 +0530

    Update 41.First Missing Positive.java

--- algorithms/41.First Missing Positive.java
+++ algorithms/41.First Missing Positive.java
@@ -1,12 +1,23 @@
 class Solution {
-    public int firstMissingPositive(int[] a) {
-        int i,n=a.length;
-        for(i=0;i<n;i++)
-        while(a[i]>0 && a[i]<=n && a[a[i]-1]!=a[i])
-        a[i]=a[i]^a[a[i]-1]^(a[a[i]-1]=a[i]);
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length,a[]=new int[n+1],i;
+        for(i=1;i<=n;i++) a[i]=nums[i-1];
         
-        for(i=0;i<n;i++)
-        if(a[i]!=i+1) return i+1;
-        return n+1;
+        for(i=1;i<=n;i++)
+        if(a[i]<=0 || a[i]>n) a[i]=0;
+        else if(a[i]!=i)
+        {
+            int from=i,to=a[i];
+            if(a[to]!=to)
+            {
+                a[from]=a[from]^a[to]^(a[to]=a[from]);
+                i--;
+            }
+            else a[i]=0;
+        }
+        
+        for(i=1;i<=n;i++)
+        if(a[i]==0) return i;
+        return i;
     }
 }
